cb996ceacc737aaa1afa7c73da80c81acc5da3e2
ylliez <90415272+ylliez@users.noreply.github.com>
2022-02-08 18:08:42 -0500

    uhoh

---
+++ telomatic/js/BLE.js
@@ -0,0 +1,38 @@
+// // The serviceUuid must match the serviceUuid of the device you would like to connect
+// const serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+// let myCharacteristic;
+// let input;
+// let myBLE;
+//
+// function setup() {
+//   myBLE = new p5ble();
+//
+//   // Create a 'Connect' button
+//   const connectButton = createButton('Connect')
+//   connectButton.mousePressed(connectToBle);
+//
+//   // Create a text input
+//   input = createInput();
+//
+//   // Create a 'Write' button
+//   const writeButton = createButton('Write');
+//   writeButton.mousePressed(writeToBle);
+// }
+//
+// function connectToBle() {
+//   // Connect to a device by passing the service UUID
+//   myBLE.connect(serviceUuid, gotCharacteristics);
+// }
+//
+// function gotCharacteristics(error, characteristics) {
+//   if (error) console.log('error: ', error);
+//   console.log('characteristics: ', characteristics);
+//   // Set the first characteristic as myCharacteristic
+//   myCharacteristic = characteristics[0];
+// }
+//
+// function writeToBle() {
+//   const inputValue = input.value();
+//   // Write the value of the input to the myCharacteristic
+//   myBLE.write(myCharacteristic, inputValue);
+// }
