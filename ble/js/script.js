642bd0f6355f3b74a16842d8a351fa8aa17812fe
ylliez <90415272+ylliez@users.noreply.github.com>
2022-02-08 14:48:46 -0500

    retry connect/disconnect

--- ble/js/script.js
+++ ble/js/script.js
@@ -1,7 +1,28 @@
+// from https://editor.p5js.org/jingwen_zhu/sketches/R3-iiTn8H
+
+// The serviceUuid must match the serviceUuid of the device you would like to connect
+/* invalid UUIDs
+const serviceUuid = "19b10010-e8f2-537e-4f6c-d104768a1214";
+const serviceUuid = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+// device information (primary service)
+const serviceUuid = "0x180A";
+const serviceUuid = "0x180a";
+// generic access
+const serviceUuid = "0x1800";
+// UART alias?
+const serviceUuid = "0x2900";
+*/
+
+/* valid UUIDs
+// generic access
+const serviceUuid = "00002a00-0000-1000-8000-00805f9b34fb";
+// UART
 const serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-let myCharacteristic;
-let myValue = 0;
+*/
+
+const serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 let myBLE;
+let isConnected = false;
 
 function setup() {
   // Create a p5ble class
@@ -12,8 +33,12 @@ function setup() {
   textAlign(CENTER, CENTER);
 
   // Create a 'Connect' button
-  const connectButton = createButton("Connect");
+  const connectButton = createButton('Connect')
   connectButton.mousePressed(connectToBle);
+
+  // Create a 'Disconnect' button
+  const disconnectButton = createButton('Disconnect')
+  disconnectButton.mousePressed(disconnectToBle);
 }
 
 function connectToBle() {
@@ -21,25 +46,36 @@ function connectToBle() {
   myBLE.connect(serviceUuid, gotCharacteristics);
 }
 
-// A function that will be called once got characteristics
-function gotCharacteristics(error, characteristics) {
-  if (error) console.log("error: ", error);
-  console.log("characteristics: ", characteristics);
-  myCharacteristic = characteristics[0];
-  // Read the value of the first characteristic
-  myBLE.read(myCharacteristic, gotValue);
+function disconnectToBle() {
+  // Disonnect to the device
+  myBLE.disconnect();
+  // Check if myBLE is connected
+  isConnected = myBLE.isConnected();
 }
 
-// A function that will be called once got values
-function gotValue(error, value) {
-  if (error) console.log("error: ", error);
-  console.log("value: ", value);
-  myValue = value;
-  // After getting a value, call p5ble.read() again to get the value again
-  myBLE.read(myCharacteristic, gotValue);
+function onDisconnected() {
+  console.log('Device got disconnected.');
+  isConnected = false;
+}
+
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+  if (error) console.log('error: ', error);
+  console.log('characteristics: ', characteristics);
+
+  // Check if myBLE is connected
+  isConnected = myBLE.isConnected();
+
+  // Add a event handler when the device is disconnected
+  myBLE.onDisconnected(onDisconnected)
 }
 
 function draw() {
-  background(250);
-  text(myValue, 100, 100);
+  if (isConnected) {
+    background(0, 255, 0);
+    text('Connected!', 100, 100);
+  } else {
+    background(255, 0, 0);
+    text('Disconnected :/', 100, 100);
+  }
 }
