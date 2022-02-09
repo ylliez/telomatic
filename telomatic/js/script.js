fd05a558049f997f94a893eef2235155b3c20269
ylliez <90415272+ylliez@users.noreply.github.com>
2022-02-08 19:10:48 -0500

    fixed

--- telomatic/js/script.js
+++ telomatic/js/script.js
@@ -1,30 +1,55 @@
-// The serviceUuid must match the serviceUuid of the device you would like to connect
+/* general */
+"use strict";
+
+// program state (loading, running)
+let state = `loading`;
+
+/* ble */
 const serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-let myCharacteristic;
-let input;
-let myBLE;
+let myBLE, myCharacteristic;
+let connectButton, onButton, offButton;
+
+/* ml5 */
+// user webcam feed
+let video;
+// handpose object
+let handpose;
+// current set of predictions made by Handpose once it's running
+let predictions = [];
+let index;
+// graphics elements for drawing overlay
+let trailBlazer;
 
 function setup() {
+  /* ble setup */
   myBLE = new p5ble();
-
   createBleButtons();
 
+  /* ml5 setup */
+  createCanvas(640, 480);
+  // Start webcam and hide the resulting HTML element
+  video = createCapture(VIDEO);
+  video.hide();
+  // start the Handpose model and switch to our running state when it loads
+  handpose = ml5.handpose(video, { flipHorizontal: true }, () => { state = `running`; });
+  // listen for prediction events from Handpose and store the results in our predictions array when they occur
+  handpose.on(`predict`, (results) => { predictions = results; });
+  // index finger object
+  index = new Index();
+  // holder for graphics overlay
+  trailBlazer = createGraphics(width, height);
 }
 
 function createBleButtons() {
   // connect button
-  const connectButton = createButton('Connect');
-  connectButton.position(10, 10);
+  connectButton = createButton('Connect')
   connectButton.mousePressed(connectToBle);
-
-  // text input
-  input = createInput();
-  input.position(10, 30);
-
-  // write button
-  const writeButton = createButton('Write');
-  writeButton.mousePressed(writeToBle);
-  writeButton.position(10, 50);
+  // on button
+  onButton = createButton('On');
+  onButton.mousePressed(bleOn);
+  // off button
+  offButton = createButton('Off');
+  offButton.mousePressed(bleOff);
 }
 
 function connectToBle() {
@@ -39,8 +64,57 @@ function gotCharacteristics(error, characteristics) {
   myCharacteristic = characteristics[0];
 }
 
-function writeToBle() {
-  const inputValue = input.value();
-  // Write the value of the input to the myCharacteristic
-  myBLE.write(myCharacteristic, inputValue);
+function bleOn() { myBLE.write(myCharacteristic, 255); }
+
+function bleOff() { myBLE.write(myCharacteristic, 0); }
+
+/**
+Handles the two states of the program: loading, running
+*/
+function draw() {
+  switch (state) {
+    case `loading`: loading(); break;
+    case `running`: running(); break;
+  }
+}
+
+/**
+Displays a simple loading screen with the loading model's name
+*/
+function loading() {
+  background(255);
+
+  push();
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(`Loading...`, width / 2, height / 2);
+  pop();
+}
+
+/* displays webcam feed, if hand found, draws path at index fingertip */
+function running() {
+  // Display mirror webcam feed
+  let flippedVideo = ml5.flipImage(video);
+  image(flippedVideo, 0, 0, width, height);
+
+  // startButton.draw();
+
+  // Check if there currently predictions to display
+  if (predictions.length > 0) {
+    index.coordinates = predictions[0];
+    index.coordinate();
+  }
+  drawIndexTip();
+}
+
+//
+function drawIndexTip() {
+  trailBlazer.push();
+  trailBlazer.stroke(255,0,0);
+  trailBlazer.strokeWeight(3);
+  // console.log(`${index.prev.x}, ${index.prev.y}, ${index.tip.x}, ${index.tip.y}`);
+  trailBlazer.line(index.prev.x, index.prev.y, index.tip.x, index.tip.y);
+  trailBlazer.pop();
+  image(trailBlazer, 0, 0);
 }
